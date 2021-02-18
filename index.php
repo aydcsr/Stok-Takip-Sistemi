b901978dcb855c0d94191c08942800268ed17bbb
Aydın CESUR <aydcsrmail.com>
2021-02-18 20:31:23 +0300

    gitpatch1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+require_once 'init.php';
+$system = new system();
+?>
---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'core/model.php';
+require_once 'core/controller.php';
+require_once 'core/view.php';
+require_once 'config/default.php';
+require_once 'core/system.php';
+require_once 'config/helper.php';
+require_once 'config/sessionManager.php';
+?>
